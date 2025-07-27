6cee1b408220b9e9a84f689e34f2fc457b4d2fea
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2025-07-27 21:30:34 +0800

    Create CombinedNotes.swift

---
+++ CombinedNotes.swift
@@ -0,0 +1,25 @@
+
+
+// Data types
+"""
+Character - A 16-bit unicode character - "Fried"
+String - Represents textual data - "Hello world"
+Int (Integer) - An integer number - 6
+Float - Decimal with max char limit of 32
+Double (use this instead of floats) - Deciaml with max char limit of 64 - 2.422342412414
+Bool - Any 2 values - True or False
+"""
+
+// Math - only works for integers, integer interpolated doubles and floats 
+var number0 = 1
+var number1 = 2
+var output = number0 * number1
+var output1 = number0 / number1
+var output2 = number0 + number1
+var output3 = number0 - number1 
+print(output)
+print(output1)
+print(output2)
+print(output3)
+
+// Booleans - True or false 
