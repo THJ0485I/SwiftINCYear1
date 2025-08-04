f2901eafe217ea00131487b6f948a826562c7c3b
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2025-08-04 16:43:01 +0800

    Update Lesson4.swift

--- Lesson4.swift
+++ Lesson4.swift
@@ -40,3 +40,30 @@ for club in clubs {
     print("\(club.topPlayer) is the top player of \(club.name)")
 }
 
+struct FootballPlayer {
+    let name: String
+    let position: String
+    let goals: Int
+    let assists: Int
+
+    // Method to describe the player
+    func describe() -> String {
+        
+        // change return"..." to whatever you want. just need to include the parameters (e.g. \(position)) parameters are below
+        return "\(name) plays as a \(position) with \(goals) goals and \(assists) assists."
+    }
+}
+let players = [
+    // change the parameters e.g. movie: kpop demon hunters. can add integers and strings
+    FootballPlayer(name: "Messi", position: "Right Wing", goals: 820, assists: 360),
+    FootballPlayer(name: "C. Ronaldo", position: "Left Wing / Striker", goals: 870, assists: 250),
+    FootballPlayer(name: "De Bruyne", position: "Central Midfielder", goals: 100, assists: 270),
+    FootballPlayer(name: "Haaland", position: "Striker", goals: 210, assists: 40),
+    FootballPlayer(name: "Neymar Jr", position: "Left Wing", goals: 305, assists: 190)
+]
+
+for player in players {
+    print(player.describe())
+}
+
+
