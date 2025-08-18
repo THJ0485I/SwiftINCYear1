cc605d1a53c1eba00510a32c62c915e21f086a77
HongJiang47_YT <94294806+THJ0485I@users.noreply.github.com>
2025-08-18 20:29:22 +0800

    Update Lesson6.swift

--- Lesson6.swift
+++ Lesson6.swift
@@ -30,3 +30,110 @@ struct ContentView: View {
 #Preview {
     ContentView()
 }
+
+
+///////////////
+
+//
+//  ContentView.swift
+//  LessonUse
+//
+//  Created by Tung Hong Jiang on 18/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var val = 0
+    var body: some View {
+        NavigationStack {
+            VStack {
+                List {
+                    Stepper("Ethan: \(val)", value: $val, step: 100000000)
+                    
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+//
+//  ContentView.swift
+//  LessonUse
+//
+//  Created by Tung Hong Jiang on 18/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var val = 0
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer() // push to the botto
+                HStack {
+                    Spacer() // Push to the right
+                    Text("Align to the right")
+                    
+                }
+                .background(Color.red)
+                HStack{
+                    Text("Align to the left") // push to the left
+                    Spacer()
+                }
+                .background(Color.blue)
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
+//
+//  ContentView.swift
+//  LessonUse
+//
+//  Created by Tung Hong Jiang on 18/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("Minions")
+                    .padding(.top)
+                    .font(.headline)
+                    .contextMenu {
+                        Button {
+                            
+                        } label: {
+                            Label("Eat", systemImage: "heart")
+                        }
+                        Button {
+                            
+                        } label: {
+                            Label("Drink", systemImage: "pencil")
+                        }
+                    }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+
